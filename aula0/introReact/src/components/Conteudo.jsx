8a312200ca33d54fd9e72a73790a08d287d9100b
danielibordulis <danieli_b_silva@estudante.sesisenai.org.br>
2024-08-06 17:18:00 -0300

    aula react

--- aula0/introReact/src/components/Conteudo.jsx
+++ aula0/introReact/src/components/Conteudo.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+
 import './Conteudo.css'
+import {Coracao} from './Coracao'
+import Navbar from './Navbar'
 
 function Conteudo() {
   return (
     <div className='conteudo-container'>
+    
+       <img className='imagem-conteudo'src="./images/taylor.png" alt='imagem da taylor em preto e branco'/>
+
       <p>jnvfnvifnvoifnvofinvodfinvsdonvi~dfvinsdfvnfdsivnfdinv
         fvunfdnvfodivnofidnvfiodnvsdfnvfdonivosidnvsodinvsndvisn
       </p>
+      <Coracao/>
+   
     </div>
   )
 }
